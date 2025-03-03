cdce7bb823f42a9d49bb2bd5e1072f96a8f8df36
daidai411411 <daidai411411@gmail.com>
2025-03-04 01:57:13 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((c) => c + 1);
+  }
+
   return (
     <>
-      <p>This is daidai411.com</p>
+      <p>This is daidai411.com.</p>
+      <button onClick={increment}>Click me</button>
+      <p>Count: {count}</p>
     </>
   );
 }
